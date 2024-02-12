5cf1c6037c34f64b4e47293380b13d6609825498
Ammarmyp <ammarmyp@gmail.com>
2024-02-12 16:20:36 +0300

    final touch, player page linked

--- MUSIC_WEBSITE/script/scroll.js
+++ MUSIC_WEBSITE/script/scroll.js
@@ -8,15 +8,18 @@ ScrollReveal({
   delay: 400,
 });
 
-ScrollReveal().reveal(".home .content", { delay: 600, origin: "top" });
+ScrollReveal().reveal(".bigHeadline", {
+  delay: 500,
+  origin: "top",
+});
 ScrollReveal().reveal(".about .content , .about .heading", {
   delay: 600,
   origin: "left",
 });
-ScrollReveal().reveal(".artist .heading ", { delay: 700, origin: "left" });
-ScrollReveal().reveal(".btn", { delay: 800, origin: "left" });
-ScrollReveal().reveal(".about .row .image ", { delay: 700, origin: "bottom" });
+ScrollReveal().reveal(".artist .heading ", { delay: 600, origin: "left" });
+ScrollReveal().reveal(".btn", { delay: 700, origin: "left" });
+ScrollReveal().reveal(".about .row .image ", { delay: 600, origin: "bottom" });
 ScrollReveal().reveal(".artist .box-container .box ", {
-  delay: 700,
+  delay: 600,
   origin: "left",
 });

d822bf37c8639347d5df65ab88e38af465a2eea0
Ammarmyp <ammarmyp@gmail.com>
2024-02-12 14:27:35 +0300

    including the player files

---
+++ Wheel_Of_Fortune/wheel2.js
@@ -0,0 +1,121 @@
+
+function rotate () {
+    const wheel = document.querySelector('.wheel');
+    const startButton = document.querySelector('.button');
+    const display = document.querySelector('.display');
+    const scoreDisplay = document.querySelector('.score');
+    const guess = document.querySelector('#userGuess')
+
+    // setting degree on the wheel
+    let deg = 0;
+    let sectionSize = 45; //deg of the partitions
+    let score = 0;
+    let gamesPlayed = 0;
+
+    const sectionItems = {
+        1: "Frog",
+        2: "Snail",
+        3: "Dolphin",
+        4: "Ladybug",
+        5: "Koala",
+        6: "Unicorn",
+        7: "Dragon",
+        8: "Snowman"
+    }
+
+    
+
+    const handleWin = (actualDeg, guess) => {
+        const winningFigureNum = Math.ceil(actualDeg / sectionSize);
+        const winningFigure = sectionItems[winningFigureNum];
+
+        display.innerHTML = winningFigure;
+        if(winningFigure.toLowerCase() === guess.toLowerCase()){
+            // increment score if guess matches
+            score++;
+        }else{
+            // decrement score if not
+            score--;
+        }
+
+        // track number of games and update score
+        gamesPlayed++;
+        scoreDisplay.innerHTML = `Score: ${score}`;
+
+        const oneGame = 4;
+        const rewardNum = 2;
+        if(gamesPlayed ===  oneGame) {
+            // action after 10 games
+            if (score >= rewardNum){
+                // reward for winning
+                console.log("winner")
+            } else {
+                // no reward
+                console.log("losser")
+                document.body.style.backgroundImage = 'url("./images/loser.png")';
+                document.body.style.opacity = '0.7'
+                restartButton.style.display = 'block';
+            }
+
+            gamesPlayed = 0;
+            score = 0;
+        }
+
+    }
+
+
+
+    startButton.addEventListener('click', () => {
+        //Reset the display
+        display.innerHTML = "--"
+            //to deactivate the pointer event while the pointer is spinning
+            startButton.style.pointerEvents = 'none' ; 
+
+            //calculate the rotation
+            deg = Math.floor(5000 + Math.random() * 5000);
+            // transition to make the wheel slow down at the end
+            wheel.style.transition = 'all 10s ease-out'; 
+            wheel.style.transform = `rotate(${deg}deg)`
+
+            // blur effect activation
+
+            wheel.classList.add('blur');
+
+    });
+
+    //event for doing things after the spinning ends
+    wheel.addEventListener('transitionend', () =>{
+        wheel.classList.remove('blur');
+        startButton.style.pointerEvents = 'auto';
+        restartButton.style.pointerEvents = 'auto'
+        restartButton.textContent = 'Restart the Game'
+        wheel.style.transition = 'none';
+
+        // instantly change the degree in between 0-360
+        //meaning making the deg in the scale of a circle(360)
+        const actualDeg = deg % 360;
+        wheel.style.transform = `rotate(${actualDeg}deg)`;
+
+        //calculate the winning figure
+
+        handleWin(actualDeg, guess.value);
+
+    })
+};
+
+const restartButton = document.querySelector('.restart');
+    restartButton.style.display = ''; // Initially hide the button
+    restartButton.style.pointerEvents = 'none'
+
+    // Function to restart the game by reloading the page
+    const restartGame = () => {
+        restartButton.textContent = 'Restart Game';
+        location.reload();
+    };
+
+    // Append the restart button to the body
+
+    // Event listener for the restart button
+    restartButton.addEventListener('click', restartGame);
+
+rotate();
---
+++ player/part2.js
@@ -0,0 +1,181 @@
+
+// search data start 
+let search_results = document.getElementsByClassName('search_results')[0];
+
+songs.forEach(element => {
+    const {id, songName, poster} = element;
+    // console.log(poster);
+    let card = document.createElement('a');
+    card.classList.add('card');
+    card.href = "#" + id;
+    card.innerHTML = `
+    <img src="${poster}" alt="">
+                            <div class="content">
+                                ${songName}
+                            </div>
+    `;
+    search_results.appendChild(card);
+});
+
+
+let input = document.getElementsByTagName('input')[0];
+
+input.addEventListener('keyup', ()=>{
+    let input_value = input.value.toUpperCase();
+    let items = search_results.getElementsByTagName('a');
+
+    for (let index = 0; index < items.length; index++) {
+        let as = items[index].getElementsByClassName('content')[0];
+        let text_value = as.textContent || as.innerHTML;
+
+        if (text_value.toUpperCase().indexOf(input_value) > -1) {
+            items[index].style.display = "flex";
+        } else {
+            items[index].style.display = "none";
+        }
+
+        if (input.value == 0) {
+            search_results.style.display = "none";
+        } else {
+            search_results.style.display = "";
+        }
+        
+    }
+})
+// search data end 
+
+
+
+let shuffle = document.getElementsByClassName('shuffle')[0];
+
+shuffle.addEventListener('click', ()=>{
+    let a = shuffle.innerHTML;
+
+    switch (a) {
+        case "next":
+            shuffle.classList.add('bi-arrow-repeat');
+            shuffle.classList.remove('bi-music-note-beamed');
+            shuffle.classList.remove('bi-shuffle');
+            shuffle.innerHTML = 'repeat';
+            break;
+    
+       case "repeat": 
+       shuffle.classList.remove('bi-arrow-repeat');
+            shuffle.classList.remove('bi-music-note-beamed');
+            shuffle.classList.add('bi-shuffle');
+            shuffle.innerHTML = 'random';
+            break;
+       case "random": 
+       shuffle.classList.remove('bi-arrow-repeat');
+            shuffle.classList.add('bi-music-note-beamed');
+            shuffle.classList.remove('bi-shuffle');
+            shuffle.innerHTML = 'next';
+            break;
+    }
+});
+
+
+
+const next_music = () => {
+    if (index == songs.length) {
+        index = 1
+    } else {
+        index++;
+    }
+    music.src = `audio/${index}.mp3`;
+    poster_master_play.src = `img/${index}.jpg`;
+    music.play();
+    masterPlay.classList.remove('bi-play-fill');
+    masterPlay.classList.add('bi-pause-fill');
+    download_music.href =  `audio/${index}.mp3`;
+    let songTitles = songs.filter((els) => {
+        return els.id == index;
+    });
+
+    songTitles.forEach(elss => {
+        let { songName } = elss;
+        title.innerHTML = songName;
+        download_music.setAttribute('download', songName);
+    });
+
+    makeAllBackground();
+    Array.from(document.getElementsByClassName('songItem'))[index - 1].style.background = "rgb(105, 105, 105, .1)";
+    makeAllplays();
+    el.target.classList.remove('bi-play-circle-fill');
+    el.target.classList.add('bi-pause-circle-fill');
+    wave.classList.add('active1');
+}
+const repeat_music = () => {
+    index;
+    music.src = `audio/${index}.mp3`;
+    poster_master_play.src = `img/${index}.jpg`;
+    music.play();
+    masterPlay.classList.remove('bi-play-fill');
+    masterPlay.classList.add('bi-pause-fill');
+    download_music.href =  `audio/${index}.mp3`;
+    let songTitles = songs.filter((els) => {
+        return els.id == index;
+    });
+
+    songTitles.forEach(elss => {
+        let { songName } = elss;
+        title.innerHTML = songName;
+        download_music.setAttribute('download', songName);
+    });
+
+    makeAllBackground();
+    Array.from(document.getElementsByClassName('songItem'))[index - 1].style.background = "rgb(105, 105, 105, .1)";
+    makeAllplays();
+    el.target.classList.remove('bi-play-circle-fill');
+    el.target.classList.add('bi-pause-circle-fill');
+    wave.classList.add('active1');
+}
+
+const random_music = () => {
+    if (index == songs.length) {
+        index = 1
+    } else {
+        index = Math.floor((Math.random() * songs.length) + 1);
+    }
+    music.src = `audio/${index}.mp3`;
+    poster_master_play.src = `img/${index}.jpg`;
+    music.play();
+    masterPlay.classList.remove('bi-play-fill');
+    masterPlay.classList.add('bi-pause-fill');
+    download_music.href =  `audio/${index}.mp3`;
+    let songTitles = songs.filter((els) => {
+        return els.id == index;
+    });
+
+    songTitles.forEach(elss => {
+        let { songName } = elss;
+        title.innerHTML = songName;
+        download_music.setAttribute('download', songName);
+    });
+
+    makeAllBackground();
+    Array.from(document.getElementsByClassName('songItem'))[index - 1].style.background = "rgb(105, 105, 105, .1)";
+    makeAllplays();
+    el.target.classList.remove('bi-play-circle-fill');
+    el.target.classList.add('bi-pause-circle-fill');
+    wave.classList.add('active1');
+}
+
+
+
+music.addEventListener('ended', ()=>{
+    let b = shuffle.innerHTML;
+
+    switch (b) {
+        case 'repeat':
+            repeat_music();
+            break;
+        case 'next':
+            next_music();
+            break;
+        case 'random':
+            random_music();
+            break;
+    }
+})
+
